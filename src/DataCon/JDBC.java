02de2406e202b298c12a7c60a39ec15faed07973
nhane0105 <149144700+nhane0105@users.noreply.github.com>
2024-04-06 17:57:44 +0700

    fix: ket noi database

--- src/DataCon/JDBC.java
+++ src/DataCon/JDBC.java
@@ -4,13 +4,14 @@ import java.sql.*;
 public class JDBC {
     public static Connection getConnection(){
         Connection c = null;
-        String url = "jdbc:sqlserver://LAPTOP-VNOPB5Q7\\SQLEXPRESS:1433; databaseName=Hello;user=sa;password=123;"
-                + "encrypt=true;trustServerCertificate=true";
-        
+         String dbUrl = "jdbc:sqlserver://localhost:1433;databaseName=leatherManagerSystem1;trustServerCertificate=true";
+            String username = "sa";
+            String password = "12345";
+          
         if( c == null){
             try {
                 //step 1: craete connection
-                c = DriverManager.getConnection(url);
+               c = DriverManager.getConnection(dbUrl, username, password);
 //                System.out.println("Connection is established with " + c.getCatalog());
 
             } catch (SQLException ex) {
@@ -31,4 +32,7 @@ public class JDBC {
         }
         
     } 
+    public static void main(String[] args) {
+        Connection c = getConnection();
+    }
 }
